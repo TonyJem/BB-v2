69cc3eaacab239d8846a05c74200a8fd84c05621
TonyJem <a.jemanov@gmail.com>
2021-03-04 01:46:53 +0200

    Create UI and Tempalte of "CharacterDetailsView" XIB

--- BB/BB/Characters Flow/CharacterDetailsView/CharacterDetailsView.swift
+++ BB/BB/Characters Flow/CharacterDetailsView/CharacterDetailsView.swift
@@ -1,6 +1,37 @@
 import UIKit
 
 class CharacterDetailsView: UIView {
-
-
+    
+    @IBOutlet weak private var nameLabel: UILabel!
+    @IBOutlet weak private var birthdayLabel: UILabel!
+    @IBOutlet weak private var quotesTableView: UITableView!
+    
+    private var quotesView: UIView!
+    private var xibName: String = "CharacterDetailsView"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setQuotesView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setQuotesView()
+    }
+    
+    private func setQuotesView() {
+        quotesView = getFromXib()
+        quotesView.frame = bounds
+        quotesView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+        addSubview(quotesView)
+    }
+    
+    private func getFromXib() -> UIView {
+        let bundle = Bundle(for: type(of: self))
+        let xib = UINib(nibName: xibName, bundle: bundle)
+        guard let view = xib.instantiate(withOwner: self, options: nil).first as? UIView else {return UIView()}
+        return view
+    }
+    
 }
