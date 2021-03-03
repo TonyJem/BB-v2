ff9dc423244429e3886b4913b7e7deb0cb42a8ae
TonyJem <a.jemanov@gmail.com>
2021-03-03 22:01:11 +0200

    Add "EpisodeDetailsHeader" class functionality

--- BB/BB/Episodes Flow/EpisodeCell/EpisodeTableViewCell.swift
+++ BB/BB/Episodes Flow/EpisodeCell/EpisodeTableViewCell.swift
@@ -6,10 +6,6 @@ class EpisodeTableViewCell: UITableViewCell {
     @IBOutlet private weak var episodeTitleLabel: UILabel!
     @IBOutlet private weak var episodeNumberTitle: UILabel!
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-    }
-    
     func fillContent(selectedEpisode: Episode) {
         episodeTitleLabel.text = selectedEpisode.title
         episodeNumberTitle.text = "episode \(selectedEpisode.episodeNumber)"
--- BB/BB/Episodes Flow/EpisodeDetailsHeader/EpisodeDetailsHeader.swift
+++ BB/BB/Episodes Flow/EpisodeDetailsHeader/EpisodeDetailsHeader.swift
@@ -2,4 +2,18 @@ import UIKit
 
 class EpisodeDetailsHeader: UITableViewCell {
     
+    @IBOutlet weak var headerContainerView: UIView!
+    @IBOutlet weak var episodeTitleLabel: UILabel!
+    @IBOutlet weak var seasonAndEpisodeNumberLabel: UILabel!
+    @IBOutlet weak var airDateLabel: UILabel!
+    
+    func fillContent(for episode: Episode) {
+        episodeTitleLabel.text = episode.title
+        
+        let seasonNumberText = "Season \(episode.seasonNumber), "
+        let episodeNumberText = "Episode \(episode.episodeNumber)"
+
+        seasonAndEpisodeNumberLabel.text = seasonNumberText + episodeNumberText
+    }
+
 }
