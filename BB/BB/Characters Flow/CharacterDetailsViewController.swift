01edc10d52bc1bf63908feed423b982dae7759cd
TonyJem <a.jemanov@gmail.com>
2021-03-05 14:52:34 +0200

    WIP Create Character Quotes

--- BB/BB/Characters Flow/CharacterDetailsViewController.swift
+++ BB/BB/Characters Flow/CharacterDetailsViewController.swift
@@ -5,27 +5,40 @@ class CharacterDetailsViewController: UIViewController {
     @IBOutlet weak private var characterDetailsContainerView: CharacterDetailsView!
     
     var character: Character?
-
+    private var model = Core.Quotes
+    private var characterQuotes: [Quote]?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         characterDetailsContainerView.quotesTableView.dataSource = self
         
-        if let character = character {
-            characterDetailsContainerView.nameLabelText = character.name
-            characterDetailsContainerView.birthdayLabelText = character.birthday
-        }
+        guard let character = character else { return }
+        characterDetailsContainerView.nameLabelText = character.name
+        characterDetailsContainerView.birthdayLabelText = character.birthday
+        characterQuotes = model.getQuotes(for: character)
+        print("🟢🟢🟢 \(String(describing: characterQuotes))")
     }
     
 }
 
 extension CharacterDetailsViewController: UITableViewDataSource {
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        guard let characterQuotes = characterQuotes else { return 1 }
+//        return 2
+        return characterQuotes.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "TestTextForQuoteTable"
+        
+//        if let characterQuotes = characterQuotes {
+//            cell.textLabel?.text = characterQuotes[indexPath.row].text
+//        } else {
+//            cell.textLabel?.text = "Character has no quotes yet..."
+//        }
+        cell.textLabel?.text = "🟢 TestCellText"
         return cell
     }
     
